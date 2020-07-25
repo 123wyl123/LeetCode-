a6b93916790ced67ac8df78e766ada80ebfd91f9
Wyl <470850082@qq.com>
2020-07-25 10:39:32 +0800

    add

---
+++ LeetCode/src/Four/Ten.java
@@ -0,0 +1,76 @@
+package Four;
+
+public class Ten {
+
+
+    public int splitArray(int[] nums, int m) {
+        int left = 0;
+        int right = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            right += nums[i];
+            if (left > nums[i]) {
+                left = nums[i];
+            }
+        }
+        while (left < right) {
+            int mid = (right - left)/ 2 + left;
+            if (search(nums, mid, m)) {
+
+                right=mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    public boolean search(int[] nums, int max, int m) {
+        int count = 0;
+        int res = 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (count+ nums[i] > max) {
+                res++;
+                count = nums[i];
+            } else {
+              count += nums[i];
+            }
+        }
+        return res<=m;
+    }
+
+
+}
+
+//    int left = 0, right = 0;
+//        for (int i = 0; i < nums.length; i++) {
+//        right += nums[i];
+//        if (left < nums[i]) {
+//        left = nums[i];
+//        }
+//        }
+//        while (left < right) {
+//        int mid = (right - left) / 2 + left;
+//        if (check(nums, mid, m)) {
+//        right = mid;
+//        } else {
+//        left = mid + 1;
+//        }
+//        }
+//        return left;
+//        }
+//
+//public boolean check(int[] nums, int x, int m) {
+//        int sum = 0;
+//        int cnt = 1;
+//        for (int i = 0; i < nums.length; i++) {
+//        if (sum + nums[i] > x) {
+//        cnt++;
+//        sum = nums[i];
+//        } else {
+//        sum += nums[i];
+//        }
+//        }
+//        return cnt <= m;
+//        }
