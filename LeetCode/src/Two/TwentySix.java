6213106c47964a8b7ecbeac5a55ee6ea247147ad
Wyl <470850082@qq.com>
2020-09-16 08:21:53 +0800

    update

---
+++ LeetCode/src/Two/TwentySix.java
@@ -0,0 +1,22 @@
+package Two;
+
+public class TwentySix {
+    public TreeNode invertTree(TreeNode root) {
+        d(root);
+        return root;
+    }
+    public TreeNode d(TreeNode root)
+    {
+        if (root==null)
+        {
+            return null;
+        }
+        TreeNode d = d(root.left);
+        TreeNode d1 = d(root.right);
+
+        root.left = d1;
+        root.right = d;
+        return root;
+
+    }
+}
