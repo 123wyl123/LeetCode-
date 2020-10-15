6c76e3c432d1d5104ec5fd4cd7691113fd97a827
Wyl <470850082@qq.com>
2020-10-15 18:50:30 +0800

    add

---
+++ LeetCode/src/One/Sixteen.java
@@ -0,0 +1,32 @@
+package One;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Sixteen {
+    public Node connect(Node root) {
+        if (root == null) {
+            return null;
+        }
+        Queue<Node> queue = new LinkedList<Node>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int n = queue.size();
+            Node last = null;
+            for (int i = 1; i <= n; ++i) {
+                Node f = queue.poll();
+                if (f.left != null) {
+                    queue.offer(f.left);
+                }
+                if (f.right != null) {
+                    queue.offer(f.right);
+                }
+                if (i != 1) {
+                    last.next = f;
+                }
+                last = f;
+            }
+        }
+        return root;
+    }
+}
