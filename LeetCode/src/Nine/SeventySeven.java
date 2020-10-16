b692ae9e5f46f7299bfb0b9b82436eb07e0c32aa
Wyl <470850082@qq.com>
2020-10-16 14:32:51 +0800

    add

---
+++ LeetCode/src/Nine/SeventySeven.java
@@ -0,0 +1,16 @@
+package Nine;
+
+public class SeventySeven {
+    public int[] sortedSquares(int[] A) {
+        int start = 0;
+        int end = A.length-1;
+        int i = end;
+        int[] B = new int[A.length];
+        while(i >= 0){
+            B[i--] = A[start]*A[start] >= A[end]*A[end]? A[start]*A[start++]:A[end]*A[end--];
+        }
+        return B;
+    }
+
+    }
+
