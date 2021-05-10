509fb9d11b536fee8acae234e149044e82c47f4a
Wyl <470850082@qq.com>
2021-05-10 22:18:43 +0800

    upup

---
+++ LeetCode/src/Acwing/Ten/ThirtyTwo.java
@@ -0,0 +1,34 @@
+package Acwing.Ten;
+
+import java.util.Scanner;
+
+public class ThirtyTwo {
+    public static void main(String[] args) {
+      
+        Scanner scanner = new Scanner(System.in);
+        int i = scanner.nextInt();
+        long sum =1;
+        long temp =1;
+        for (int j = 1; j <=i ; j++) {
+
+
+            if (sum*j%10==0)
+            {   sum=sum*j;
+               while (sum%10==0)
+               {
+                   sum=sum/10;
+               }
+               sum=sum%10;
+            }else
+            {
+                sum=sum*j;
+            }
+
+
+
+        }
+
+        System.out.println(sum%10);
+    }
+
+}
