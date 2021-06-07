f20ea1f7f6067776f62134f2bf38ddc5bc1851d0
Wyl <470850082@qq.com>
2021-06-07 09:26:06 +0800

    leetcode

--- LeetCode/src/Four/NinetyFour.java
+++ LeetCode/src/Four/NinetyFour.java
@@ -29,6 +29,45 @@ public class NinetyFour {
         }
     }
 
+
+    public int findTargetSumWays1(int[] nums, int target) {
+    int sum =0;
+    int length = nums.length;
+        for(int i = 0;i<length;i++)
+    {
+        sum+=Math.abs(nums[i]);
+    }
+        if(Math.abs(sum)<Math.abs(target))
+    {
+        return 0;
+    }
+    int dp[][]= new int[length][sum*2+1];
+    dp[length-1][-nums[length-1]+sum]+=1;
+    dp[length-1][nums[length-1]+sum]+=1;
+            for(int i =length-2;i>=0;i--)
+    {
+        for(int j =0;j<2*sum+1;j++)
+        {
+
+            if(j>=nums[i]&&j-nums[i]<2*sum+1)
+            {
+                dp[i][j]+=dp[i+1][j-nums[i]];
+            }
+            if(j+nums[i]>=0&&j+nums[i]<2*sum+1)
+            {
+                dp[i][j]+=dp[i+1][j+nums[i]];
+            }
+
+
+
+
+        }
+    }
+            return dp[0][target+sum];
+}
+
+
+
     public static void main(String[] args) {
         int a[] = {1, 1, 1, 1, 1};
         new NinetyFour().findTargetSumWays(a, 3);
--- LeetCode/src/TwoFourFour/Three.java
+++ LeetCode/src/TwoFourFour/Three.java
@@ -2,6 +2,42 @@ package TwoFourFour;
 
 public class Three {
     public int minFlips(String s) {
+        int n = s.length();
+        int[][] l = new int[n][2];
+
+        int ans = 0;
+
+        l[0][0] = (s.charAt(0) == '0') ? 0 : 1;
+        l[0][1] = (s.charAt(0) == '1') ? 0 : 1;
+        for (int i = 1; i < s.length(); i++) {
+            l[i][0] = ((s.charAt(i) == '0') ? 0 : 1) + l[i - 1][1];
+            l[i][1] = ((s.charAt(i) == '1') ? 0 : 1) + l[i - 1][0];
+        }
+
+        int[][] r = new int[n][2];
+        r[n - 1][0] = (s.charAt(n - 1) == '0') ? 0 : 1;
+        r[n - 1][1] = (s.charAt(n - 1) == '1') ? 0 : 1;
+        for (int i = n - 2; i >= 0; i--) {
+            r[i][0] = ((s.charAt(i) == '0') ? 0 : 1) + r[i + 1][1];
+            r[i][1] = ((s.charAt(i) == '1') ? 0 : 1) + r[i + 1][0];
+        }
+
+        ans = Math.min(l[n - 1][0], l[n - 1][1]);
+        for (int i = 1; i < n; i++) {
+            if ((n - i) % 2 == 0) {
+                // 0101 0101 ? 1010 1010
+                ans = Math.min(ans, r[i][0] + ((i % 2 == 0) ? l[i - 1][1] : l[i - 1][0]));
+                ans = Math.min(ans, r[i][1] + ((i % 2 == 0) ? l[i - 1][0] : l[i - 1][1]));
+            } else {
+                // 101 0101
+                ans = Math.min(ans, r[i][0] + ((i % 2 == 0) ? l[i - 1][0] : l[i - 1][1]));
+                ans = Math.min(ans, r[i][1] + ((i % 2 == 0) ? l[i - 1][1] : l[i - 1][0]));
+            }
+        }
+
+        return ans;
+    }
+    public int minFlips1(String s) {
         StringBuilder sb1 = new StringBuilder();
         StringBuilder sb2 = new StringBuilder();
         int n = s.length();
