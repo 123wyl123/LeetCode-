d21ba55407eeb1b6251bf4bd829c186f706c13f4
Wyl <470850082@qq.com>
2021-07-12 09:56:46 +0800

    leetcode

---
+++ LeetCode/src/Five/TwentySix.java
@@ -0,0 +1,34 @@
+package Five;
+
+public class TwentySix {
+    public int hIndex(int[] citations) {
+        int temp=0;
+        int length = citations.length;
+
+        int l=1;
+        int r=citations[length-1];
+        while (l<=r)
+        {
+            int atemp = (r+l+1)/2;
+            int dfs = dfs(citations,atemp);
+            if (length-dfs>=atemp)
+            {   temp=Math.max(atemp,atemp);
+                l=atemp+1;
+            }else
+            {
+                r=atemp-1;
+            }
+        }
+        return temp;
+    }
+    public int dfs (int a[],int l)
+    {
+        for (int i = 0; i <a.length ; i++) {
+            if (a[i]>=l)
+            {
+                return i;
+            }
+        }
+        return a.length;
+    }
+}
