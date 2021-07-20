5f2031bbd3403b3f08d50ed8c5b99ddb48b18c69
Wyl <470850082@qq.com>
2021-07-20 08:42:53 +0800

    leetcode

---
+++ LeetCode/src/Eightten/SeventySeven.java
@@ -0,0 +1,14 @@
+package Eightten;
+
+import java.util.Arrays;
+
+public class SeventySeven {
+    public int minPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int max =0;
+        for (int i = 0; i <nums.length/2 ; i++) {
+            max =Math.max(nums[i]+nums[nums.length-i-1],max);
+        }
+        return max;
+    }
+}
