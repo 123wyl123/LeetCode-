aa54128e6a89e4ff8a79ce576a5c0c3c0c9e4b5d
Wyl <470850082@qq.com>
2021-07-27 09:37:50 +0800

    leetcode

---
+++ LeetCode/src/Six/SeventyOne.java
@@ -0,0 +1,27 @@
+package Six;
+
+public class SeventyOne {
+    int a =0;
+    public int findSecondMinimumValue(TreeNode root) {
+        a=root.val;
+   a=     dfs(root);
+        return a>root.val?a:-1;
+    }
+
+    public int dfs(TreeNode root)
+    {   if (root.val>a)
+    {
+        return root.val;
+    }
+       int left= dfs(root.left);
+        int right =dfs(root.right);
+        if (left>a&&right>a)
+        {
+            return Math.min(left,right);
+        }else
+        {
+            return Math.max(left,right);
+        }
+
+    }
+}
