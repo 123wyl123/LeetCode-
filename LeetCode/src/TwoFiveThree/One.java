6ae1db6a489f9b1055a853ffdf74622c992edf16
Wyl <470850082@qq.com>
2021-08-07 23:58:37 +0800

    leetcode

---
+++ LeetCode/src/TwoFiveThree/One.java
@@ -0,0 +1,30 @@
+package TwoFiveThree;
+
+public class One {
+    public String makeFancyString(String s) {
+        char[] chars = s.toCharArray();
+        int l=0;
+        int r=0;
+        StringBuffer stringBuffer = new StringBuffer();
+        while (r<s.length())
+        {
+            if (chars[l]==chars[r])
+            {
+                r++;
+                if (r-l>=2)
+                {
+
+                }else
+                {
+                    stringBuffer.append(chars[r]);
+                }
+            }else
+            {
+                l=r;
+
+            }
+        }
+        return stringBuffer.toString();
+    }
+
+}
