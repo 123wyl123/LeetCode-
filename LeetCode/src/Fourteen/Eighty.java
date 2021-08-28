acab578a9ca19d43d2e2ecb3258a70fbaa04403a
Wyl <470850082@qq.com>
2021-08-28 08:45:52 +0800

    leetcode

---
+++ LeetCode/src/Fourteen/Eighty.java
@@ -0,0 +1,16 @@
+package Fourteen;
+
+public class Eighty {
+    public int[] runningSum(int[] nums) {
+        if (nums.length==0)
+        {
+            return null;
+        }
+    int sum[]=new int[nums.length];
+    sum[0]=nums[0];
+        for (int i = 1; i <nums.length ; i++) {
+            sum[i]=sum[i-1]+nums[i];
+        }
+        return sum;
+    }
+}
