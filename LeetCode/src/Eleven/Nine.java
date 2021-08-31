771a83432b896b3d62ef01b39f86f05f871c3e5d
Wyl <470850082@qq.com>
2021-08-31 09:25:07 +0800

    leetcode

---
+++ LeetCode/src/Eleven/Nine.java
@@ -0,0 +1,24 @@
+package Eleven;
+
+public class Nine {
+    public int[] corpFlightBookings(int[][] bookings, int n) {
+        int b[]=new int[n+1];
+        int a[]=new int[n+2];
+        a[0]=0;
+        for (int i = 1; i <=n ; i++) {
+            b[i]=a[i]-a[i-1];
+        }
+        for (int i = 0; i <bookings.length ; i++) {
+            b[bookings[i][0]]+=bookings[i][2];
+            b[bookings[i][1]+1]-=bookings[i][2];
+        }
+        for (int i = 1; i <=n ; i++) {
+            a[i]=b[i]+a[i-1];
+        }
+        int c[]=new int[n];
+        for (int i = 0; i <n ; i++) {
+            c[i]=a[i+1];
+        }
+        return c;
+    }
+}
