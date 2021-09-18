941cc6b3de7419d0974793a6d50080a44b7bf988
Wyl <470850082@qq.com>
2021-09-18 20:01:26 +0800

    leetcode

---
+++ LeetCode/src/Two/NinetyTwo.java
@@ -0,0 +1,15 @@
+package Two;
+
+import java.util.TreeSet;
+
+public class NinetyTwo {
+    public boolean canWinNim(int n) {
+        boolean dp[]=new boolean[n+1];
+        if (n%4==0)
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
