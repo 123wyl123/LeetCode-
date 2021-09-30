00c0ad2202b09e02f94f64d34a9b3aaefe7f4f79
Wyl <470850082@qq.com>
2021-09-30 09:52:15 +0800

    leetcode

--- LeetCode/src/Four/ThirtySeven.java
+++ LeetCode/src/Four/ThirtySeven.java
@@ -3,12 +3,14 @@ package Four;
 import java.util.HashMap;
 
 public class ThirtySeven {
+    HashMap <Integer,Integer> hash ;
     public int pathSum(TreeNode root, int targetSum) {
+        hash =new HashMap<>();
         dfs(root,targetSum);
-
+        return hash.getOrDefault(targetSum,0);
     }
 
-    public HashMap<Integer,Integer> dfs(TreeNode root, int targetSum)
+    public HashMap<Integer,Integer> dfs(TreeNode root,int targetSum)
     {   if(root==null)
     {
         return new HashMap<Integer,Integer>() ;
@@ -19,9 +21,19 @@ public class ThirtySeven {
         HashMap<Integer,Integer> b=  new HashMap<Integer,Integer>();
         for(Integer temp: left.keySet())
         {
-            b.put(temp,b.getOrDefault(temp,0)+left.get(temp));
 
+            b.put(temp+root.val,b.getOrDefault(temp+root.val,0)+left.get(temp));
         }
+        for(Integer temp : right.keySet())
+        {
 
+            b.put(temp+root.val,b.getOrDefault(temp+root.val,0)+right.get(temp));
+        }
+        b.put(root.val,b.getOrDefault(root.val,0)+1);
+        for(Integer cur:b.keySet())
+        {
+            hash.put(cur,hash.getOrDefault(cur,0)+b.get(cur));
+        }
+        return b;
     }
 }
